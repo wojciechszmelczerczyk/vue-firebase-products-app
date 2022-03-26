c0438c7a0a3d5be324c3da8e5fd8a70c0e54ae49
Wojciech <szmelczerczykwojciech@gmail.com>
2022-03-26 12:16:24 +0100

    verify token middleware added

---
+++ src/middleware/verifyToken.js
@@ -0,0 +1,33 @@
+const { decode } = require("jsonwebtoken");
+const ManageUsersService = require("../composables/admin/ManageUsersService");
+
+module.exports = {
+  async verifyToken(to, from, next) {
+    // no auth for login path
+    if (to.path === "/login") {
+      next();
+      // otherwise verify token
+    } else {
+      // fetch token from cookie
+      const token = localStorage.getItem("token");
+
+      if (token) {
+        // intercept email from token
+        const { email } = await decode(token);
+
+        // fetch user from db with email from token if exists ok, otherwise error
+        const isCorrect = await ManageUsersService.default.getUserByEmail(
+          email
+        );
+
+        if (isCorrect) {
+          next();
+        } else {
+          next("/login");
+        }
+      } else {
+        next("/login");
+      }
+    }
+  },
+};
